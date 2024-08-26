0c0a8989d2b1e9dc8253e0f906351ab8cc3ae1fc
kwon <kyjyj6334@gmail.com>
2024-08-27 00:10:54 +0900

    commit

--- src/Level0_2.java
+++ src/Level0_2.java
@@ -69,20 +69,25 @@ public class Level0_2 {
     }
 
     //숫자의 표현
+    //다아시..
+
     public int solution13(int n) {
         int answer = 0;
 
-        int a = 0;
-        int idx = 0;
-        for (int i = 1; i <= n; i++) {
-            a += i;
-            if (a == n){
-                System.out.println(a);
-                idx++;
+        for(int i = 1;i<=n;i++){
 
+            int temp = 0;
+            for(int j = i; j<=n ;j++){
+                temp = temp + j;
+                if(temp == n){
+                    answer++;
+                    break;
+                }else if(temp > n){
+                    break;
+                }
             }
-
         }
+
 //        while (a<=n){
 //            a += a;
 //            if (a == n){
